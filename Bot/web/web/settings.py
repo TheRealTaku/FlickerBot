98fec342cf531176796e8ec41c5ea4ee3319b8ed
Lisa <liisaxu1234@gmail.com>
2017-06-23 11:17:51 -0700

    modified settings.py

--- Bot/web/web/settings.py
+++ Bot/web/web/settings.py
@@ -19,7 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-os.makedirs(STATIC_ROOT)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
