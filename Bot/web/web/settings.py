f39a03ad998be5edf18ace0335ece13e17596260
Lisa <liisaxu1234@gmail.com>
2017-06-25 14:01:53 -0700

    Revert "modified settings.py"
    This reverts commit 98fec342cf531176796e8ec41c5ea4ee3319b8ed.

--- Bot/web/web/settings.py
+++ Bot/web/web/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+os.makedirs(STATIC_ROOT)
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
