0e4b3d8bde4b605e600c124a73e3c89e0da4f918
Lisa <liisaxu1234@gmail.com>
2017-06-25 14:01:41 -0700

    Revert "added whitenoise"
    This reverts commit 3c6fdc7d4372b852b531be7aff1a552d179b9159.

--- Bot/web/web/settings.py
+++ Bot/web/web/settings.py
@@ -56,7 +56,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'web.urls'
