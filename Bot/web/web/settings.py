f07c9c1abf7de8c0348d4be6fed7342e3862a866
Lisa <liisaxu1234@gmail.com>
2017-06-25 14:02:08 -0700

    Revert "Forgot a trailing comma"
    This reverts commit a445a0b8fe5adf696e60ef329220e8f802ac1aaa.

--- Bot/web/web/settings.py
+++ Bot/web/web/settings.py
@@ -23,7 +23,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'static')
 )
 
 # Quick-start development settings - unsuitable for production
